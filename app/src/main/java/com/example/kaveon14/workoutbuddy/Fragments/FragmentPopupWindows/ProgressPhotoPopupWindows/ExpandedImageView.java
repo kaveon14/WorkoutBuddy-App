5307a6bfd33f8e3f9ed01979798ffc46bc6df0d9
Kaveon Smith <kaveon31@gmail.com>
2017-08-13 19:59:57 -0500

    added 'ExpandedImageView' class

---
+++ app/src/main/java/com/example/kaveon14/workoutbuddy/Fragments/FragmentPopupWindows/ProgressPhotoPopupWindows/ExpandedImageView.java
@@ -0,0 +1,10 @@
+package com.example.kaveon14.workoutbuddy.Fragments.FragmentPopupWindows.ProgressPhotoPopupWindows;
+
+import com.example.kaveon14.workoutbuddy.Fragments.FragmentPopupWindows.PopupWindowManager;
+
+/**
+ * Created by kaveon14 on 8/13/17.
+ */
+
+public class ExpandedImageView extends PopupWindowManager {
+}
